008cbdbcea1feedd1227e63885ddf97a2f278150
Mitchell Schofield <scho.mitch@gmail.com>
2022-09-28 12:03:38 -0700

    commit after sequelize connection problem

--- db/index.js
+++ db/index.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 //------ Database Connection Configuration With Sequelize ----------------------------------------------------
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  
 });
 
 
