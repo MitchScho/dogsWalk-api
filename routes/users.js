5c236d7d6b3959f90e18235b8e4ad6114c3faa50
Mitchell Schofield <scho.mitch@gmail.com>
2023-03-30 18:50:15 -0700

    create user endpoint to get user related to walk request

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,28 @@
 const User = require("../db/models/User");
+const WalkRequest = require("../db/models/WalkRequest");
+const Dog = require("../db/models/Dog")
 
 const router = require("express").Router();
 
 module.exports = (db) => {
-  router.get("/users", (req, res) => {
 
-    User.findAll()
-      .then((users) => {
+  router.get("/users/:id", (req, res) => {
 
-        res.json(users);
-      })
+    WalkRequest.findByPk(req.params.id, {
+      include: Dog
+    })
+      .then((walkRequest) => {
+
+        User.findOne({
+          where: {
+            id: walkRequest.userId
+          }
+        })
+        .then((user) => {
+
+          res.json(user);
+        })
+    })
       .catch(err => {
         res
           .status(500)
