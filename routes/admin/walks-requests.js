b04345a2b849c80b9746d2646dfa2463adb69783
Mitchell Schofield <scho.mitch@gmail.com>
2023-04-20 20:00:42 -0700

    Refactor admin pages to have paid for requests seperate to walk requests

--- routes/admin/walks-requests.js
+++ routes/admin/walks-requests.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const {Op} = require("sequelize");
-const {Sequelize} = require("sequelize");
-const { isAdmin } = require("../../middleware/authenticate");
+const {
+  Op
+} = require("sequelize");
+const {
+  Sequelize
+} = require("sequelize");
+const {
+  isAdmin
+} = require("../../middleware/authenticate");
 const moment = require('moment');
 moment().format();
 //-----------------------------------------------------------------------------------------
@@ -17,20 +23,38 @@ module.exports = (db) => {
   router.get("/admin/walks-requests", isAdmin, (req, res) => {
 
     WalkRequest.findAll({
-        where: {
-          [Op.or]: [{
+        where:{
               isAccepted: null
             },
-            {
-              payedFor: null
-            },
-          ]
-        },
         include: Dog
       })
       .then((walkRequests) => {
 
         res.json(walkRequests);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({
+            error: err.message
+          });
+        console.log('Query Error.....');
+      })
+  });
+
+  //-------------------------------------------------------------------------------------------------------------
+
+  router.get("/admin/unpaid-requests", isAdmin, (req, res) => {
+
+    WalkRequest.findAll({
+        where: {
+          payedFor: null
+        },
+        include: Dog
+      })
+      .then((unPaidRequests) => {
+
+        res.json(unPaidRequests);
       })
       .catch((err) => {
         res
@@ -49,6 +73,86 @@ module.exports = (db) => {
 
 
   router.put("/admin/walks-requests/:id", isAdmin, async (req, res) => {
+
+    //  const id = req.params.id;
+    //  const payload = req.body;
+
+    //  const walkRequest = await WalkRequest.findByPk(id, {
+    //    include: Dog,
+    //  });
+
+    //  const isAccepted =
+    //    payload.isAccepted !== undefined ?
+    //    payload.isAccepted :
+    //    walkRequest.isAccepted;
+    //  const payedFor =
+    //    payload.payedFor !== undefined ? payload.payedFor : walkRequest.payedFor;
+
+    //  walkRequest.isAccepted = isAccepted;
+    //  walkRequest.payedFor = payedFor;
+
+    //  await walkRequest.save();
+
+    //  const approved = walkRequest.isAccepted && payload.isAccepted;
+
+    //  if (approved) {
+    //    // Find or create a walk with the given date
+    //    const [walk, created] = await Walk.findOrCreate({
+    //      where: {
+    //        date: moment(walkRequest.date).startOf("day").toDate(), // moment to compare dates and convert to Date object
+    //      },
+    //    });
+
+    //    // Include the associated dogs
+    //    const dogs = await Dog.findAll({
+    //      where: {
+    //        id: walkRequest.dogs.map((dog) => dog.id)
+    //      },
+    //    });
+
+    //    // Add the dogs to the walk using the join table
+    //    await walk.addDogs(dogs);
+
+    //    // Send a confirmation message
+    //    const userPhone = "+12502539813"; //updatedWalk.user.phoneNumber;
+    //    const userDog = walkRequest.dogs[0].name;
+
+    //    client.messages
+    //      .create({
+    //        body: `Kelsey has accepted your dog walk request for ${userDog}.`,
+    //        from: "+13087734330",
+    //        to: userPhone,
+    //      })
+    //      .then((message) => console.log(message.sid));
+    //  } else {
+    //    // Remove the dogs from the walk using the join table
+    //    const walk = await Walk.findOne({
+    //      where: {
+    //        date: moment(walkRequest.date).startOf("day").toDate(), // moment to compare dates and convert to Date object
+    //      },
+    //    });
+
+    //    await Promise.all(
+    //      walkRequest.dogs.map((dog) =>
+    //        WalkDog.destroy({
+    //          where: {
+    //            walkId: walk.id,
+    //            dogId: dog.id,
+    //          },
+    //        })
+    //      )
+    //    );
+
+    //    // Count the number of dogs associated with the walk
+    //    const dogCount = await walk.countDogs();
+
+    //    // Delete the walk if the dog count is zero
+    //    if (dogCount === 0) {
+    //      await walk.destroy();
+    //    }
+    //  }
+
+    //  return res.json(walkRequest);
 
     const id = req.params.id;
     const payload = req.body;
@@ -71,7 +175,7 @@ module.exports = (db) => {
 
       let walk = await Walk.findOrCreate({
         where: {
-          date: moment(walkRequest.date).startOf('day').toDate(), // use moment to compare dates and convert to Date object
+          date: moment(walkRequest.date).startOf('day').toDate(), // moment to compare dates and convert to Date object
         },
         include: Dog
       });
@@ -93,17 +197,26 @@ module.exports = (db) => {
       //   .then(message => console.log(message.sid));
 
     } else {
-        const walk = await Walk.findOne({
+      const walk = await Walk.findOne({
         where: {
-          date: moment(walkRequest.date).startOf('day').toDate(), // use moment to compare dates and convert to Date object
+          date: moment(walkRequest.date).startOf('day').toDate(), // moment to compare dates and convert to Date object
         }
       })
 
       await Promise.all(walkRequest.dogs.map(dog => WalkDog.destroy({
         where: {
-        walkId: walk.id,
-        dogId: dog.id
-      }})))
+          walkId: walk.id,
+          dogId: dog.id
+        }
+      })))
+
+      // Count the number of dogs associated with the walk
+      const dogCount = await walk.countDogs();
+
+      // Delete the walk if the dog count is zero
+      if (dogCount === 0) {
+        await walk.destroy();
+      }
     }
 
     return res.json(walkRequest);
